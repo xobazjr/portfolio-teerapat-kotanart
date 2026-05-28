c3a5037b5303935a4c09fa9c2f26133dfebbcf67
xobazjr <MR.TeerapatKotanartChannel@Gmail.COM>
2026-05-28 23:29:22 +0700

    Update style CSS
    ✅ Update hover img project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -14,40 +14,42 @@ const ProjectCard = ({ element }) => {
 
     return (
         <div className="projects__card">
-            <Link
-                to={`/projects/${element.slug}`}
-                className="projects__img-wrapper"
-                onMouseEnter={(e) =>
-                    showTooltip(
-                        e,
-                        imgRef,
-                        element.type
-                    )
-                }
-                onMouseMove={moveTooltip}
-                onMouseLeave={hideTooltip}
+        <Link
+            to={`/projects/${element.slug}`}
+            className="projects__img-wrapper"
+            onMouseEnter={(e) => showTooltip(e, imgRef, element.type)}
+            onMouseMove={moveTooltip}
+            onMouseLeave={hideTooltip}
+        >
+            <img
+                ref={imgRef}
+                src={element.image}
+                alt={element.alt}
+                crossOrigin="anonymous"
+                loading="lazy"
+            />
+            <svg
+                className="projects__arrow"
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
             >
-                <img
-                    ref={imgRef}
-                    src={element.image}
-                    alt={element.alt}
-                    crossOrigin="anonymous"
-                    loading="lazy"
-                />
-                {tooltip && (
-                    <div
-                        className="projects__tooltip"
-                        style={{
-                            backgroundColor: tooltip.bg,
-                            color: tooltip.color,
-                            top: pos.y + 16,
-                            left: pos.x + 16,
-                        }}
-                    >
-                        {tooltip.text}
-                    </div>
-                )}
-            </Link>
+                <path d="M10.8101 1.96222L0.726954 12.0453L1.66171 12.9801L11.7448 2.89698L11.9344 9.4447L13.208 9.07311L13.0134 2.35278C12.9877 1.46249 12.2434 0.718185 11.3531 0.692412L4.80762 0.502924L4.43487 1.77539L10.8101 1.96222Z" fill="white" stroke="white" strokeWidth="0.542084"/>
+            </svg>
+            {tooltip && (
+                <div
+                    className="projects__tooltip"
+                    style={{
+                        backgroundColor: tooltip.bg,
+                        color: tooltip.color,
+                        top: pos.y + 16,
+                        left: pos.x + 16,
+                    }}
+                >
+                    {tooltip.text}
+                </div>
+            )}
+        </Link>
             <div className="projects__title">
                 <h3 className="text-h3">
                     {element.title}
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -70,28 +70,6 @@ const ProjectPage = () => {
         </div>
       </section>
       <Containers projects={projects} slug={project.slug}/>
-          
-      {/* <section className="container_main">
-        {project_container.map((element, index) => (
-          <div key={index}>
-            <img src={element.image} alt={element.title} />
-            <div className="container_between">
-              <div className="container_left">
-                <p>{element.title}</p>
-                <p>{element.with}</p>
-              </div>
-              <div className="container_right">
-                <Link to={`/projects/${element.slug}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32">
-                  <path d="M0 0h32v32H0z" fill="none" />
-                  <path fill="currentColor" d="m18.72 6.78l-1.44 1.44L24.063 15H4v2h20.063l-6.782 6.78l1.44 1.44l8.5-8.5l.686-.72l-.687-.72z" />
-                </svg>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
-      </section> */}
       <Footer/>
     </>
   );
